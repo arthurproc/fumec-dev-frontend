8d4d0e48b691e4d153615f6170c2a2c699dc8f01
GuilhermeBenevides <guigui_bvb@yahoo.com.br>
2021-10-03 12:55:25 -0300

    Criação do cadastro de paciente (3 paginas)

---
+++ src/app/cadastro-paciente-status/cadastro-paciente-status.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from "@angular/forms";
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+
+
+@Component({
+  selector: 'app-cadastro-paciente-status',
+  templateUrl: './cadastro-paciente-status.component.html',
+  styleUrls: ['./cadastro-paciente-status.component.css']
+})
+export class CadastroPacienteStatusComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
